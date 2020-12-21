d8cbdf9e06423fe726c04dbdce2c12a194563ed1
Alexander <alexremnev42@gmail.com>
2020-12-21 22:37:40 +0700

    routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ class AuthController extends Controller
 {
     public function auth() {
         $user_email = request('email');
-        $user = User::where('email', $user_email)->get();
+        $user = User::where('email', $user_email)->first();
 
         return ['user_id' => $user->id, "user_name" => $user->name];
     }
