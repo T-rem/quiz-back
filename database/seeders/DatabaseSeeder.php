80911700dd1d56a6ba753674d5ea2d56a57b4cf1
Alexander <alexremnev42@gmail.com>
2020-12-21 11:06:28 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,23 +18,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+        for ($i=0; $i < 10; $i++) {
+            Quiz::create([
+                'user_id' => rand(1, 10),
+                'name' => Str::random(10)
+            ]);
+        }
 
-        Quiz::create([
-            'user_id' => rand(1, 10),
-            'name' => Str::random(10)
-        ]);
-
-
-
-        Question::create([
-            'name' => Str::random(10),
-            'quiz_id' => rand(1, 10)
-        ]);
-
-        Answers::create([
-            'question_id' => rand(0, 10),
-            'name' => Str::random(10),
-            'is_correct' => rand(0, 1),
-        ]);
+        for ($i=0; $i < 10; $i++) {
+            Question::create([
+                'name' => Str::random(10),
+                'quiz_id' => rand(1, 10)
+            ]);
+        }
+        for ($i=0; $i < 20; $i++) {
+            Answers::create([
+                'question_id' => rand(0, 10),
+                'name' => Str::random(10),
+                'is_correct' => rand(0, 1),
+            ]);
+        }
     }
 }
